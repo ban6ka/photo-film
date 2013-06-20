b00d2f914118adb8df59baf6c94832c766ca60a6
sagaydak <sagaydakam@gmail.com>
2013-06-20 23:35:29 +0300

    Wizard Thirt Step - base html and js for file-upload

--- sites/all/modules/custom/photo_film_form/js/photo_film_user_music_upload_step.js
+++ sites/all/modules/custom/photo_film_form/js/photo_film_user_music_upload_step.js
@@ -1,6 +1,27 @@
 (function ($) {
     Drupal.behaviors.photo_film_themes_step = {
-        attach:function (context, settings) {
+        attach: function (context, settings) {
+
+            Drupal.initTrackUploader();
         }
+    }
+
+    Drupal.initTrackUploader = function () {
+        $("#track-upload").fileupload({
+            url: '/admin/photo-film/save-file',
+            dataType: 'json',
+            autoUpload: true,
+            acceptFileTypes: /(\.|\/)(gif|jpe?g|png)$/i,
+            maxNumberOfFiles: 1,
+            maxFileSize: 5000000, // 5 MB
+
+            dropZone: $('div.file-uploader-holder'),
+            filesContainer: $('div.user-track-info'),
+            /*drop: function (e, data) {
+                $.each(data.files, function (index, file) {
+                    alert('Dropped file: ' + file.name);
+                });
+            }*/
+        });
     }
 })(jQuery)
