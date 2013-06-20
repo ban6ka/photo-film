d605c8233aeef122f9c59cf87aab62cb0b60820a
ban6ka <ivan.goncharenko@propeople.com.ua>
2013-06-20 22:29:00 +0300

    Third step was added

--- sites/all/modules/custom/photo_film_form/js/photo_film_style_step.js
+++ sites/all/modules/custom/photo_film_form/js/photo_film_style_step.js
@@ -1,13 +1,6 @@
 (function ($) {
     Drupal.behaviors.photo_film_style_step = {
         attach: function (context, settings) {
-            $('.single-style-info').click(function () {
-                if ($(this).find('.video-src').length > 0) {
-                    if ($(this).find('.video-src').length > 0) {
-                       $('.photo-video-style-step').show().find('iframe').attr('src', $(this).find('.video-src').text());
-                    }
-                }
-            });
         }
     }
 })(jQuery)
