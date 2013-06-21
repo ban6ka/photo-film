b1a440abbb317380215768f003ed3dbbb6fddf60
sagaydak <sagaydakam@gmail.com>
2013-06-21 21:53:07 +0300

    Third Step (music) - misc js corrections

--- sites/all/modules/custom/photo_film_form/js/photo_film_user_music_upload_step.js
+++ sites/all/modules/custom/photo_film_form/js/photo_film_user_music_upload_step.js
@@ -8,21 +8,58 @@
 
     Drupal.initTrackUploader = function () {
         // todo all constant in to backend!
+        var maxFiles = 1;
         $("#track-upload").fileupload({
             url: '/admin/photo-film/music/save/file',
             dataType: 'json',
             autoUpload: true,
+            singleFileUploads: false,
             acceptFileTypes: /(\.|\/)(gif|jpe?g|png)$/i,
-            maxNumberOfFiles: 1,
+            maxNumberOfFiles: maxFiles,
             maxFileSize: 5000000, // 5 MB
+            disableValidation: false,
+            disableAudioPreview: true,
 
-            dropZone: $('div.file-uploader-holder'),
-            filesContainer: $('div.user-track-info'),
-            /*drop: function (e, data) {
-                $.each(data.files, function (index, file) {
-                    alert('Dropped file: ' + file.name);
-                });
-            }*/
+            dropZone: $('div.file-uploader-holder')
+        })
+        .bind('fileuploadadd', function (e, data) {
+                debugger;
+            var fileCount = data.files.length;
+            if (fileCount > maxFiles) {
+                alert("The max number of files is "+maxFiles);
+                return false;
+            }
+        })
+        .bind('fileuploadsubmit', function (e, data) { console.log('Processing started...'); })
+        .bind('fileuploadsend', function (e, data) {/* ... */})
+        .bind('fileuploaddone', Drupal.renderTrackFile)
+        .bind('fileuploadfail', function (e, data) { console.log('Processing ' + data.files[0].name + ' fail.'); });
+
+
+    }
+
+    Drupal.renderTrackFile = function (e, data) {
+        console.log('Processing ' + data.files[0].name + ' done.');
+        var uploadFilesBox = $("div.user-track-info");
+        $.each(data.files, function (index, file) {
+            var newFileDiv = $("<div class='uploadBox' id='fileDiv_" + file.name + "'><div class='leftEle'><a href='#' id='link_" + index + "' class='removeFile'>Remove</a></div><div class='midEle'>" + file.name + "</div></div>");
+            uploadFilesBox.append(newFileDiv);
+
+            newFileDiv.find('a').on('click', { filename: file.name, files: data.files }, function (event) {
+                event.preventDefault();
+                var uploadFilesBox = uploadFilesBox;
+                var remDiv = $(document.getElementById("fileDiv_" + event.data.filename));
+                remDiv.remove();
+                data.files.length = 0;    //zero out the files array
+            });
+
+            data.context = newFileDiv;
+        });
+
+        $('#myButton').click(function () {
+            if (data.files.length > 0) {     //only submit if we have something to upload
+                data.submit();
+            }
         });
     }
 })(jQuery)
