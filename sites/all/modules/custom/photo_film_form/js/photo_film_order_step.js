0ac1b4b20dee27d19912ad5732451fe8b2cc32ef
sagaydak <sagaydakam@gmail.com>
2013-06-30 16:03:34 +0300

    30.06.13 | Order step - Info block

--- sites/all/modules/custom/photo_film_form/js/photo_film_order_step.js
+++ sites/all/modules/custom/photo_film_form/js/photo_film_order_step.js
@@ -7,14 +7,19 @@
 
     Drupal.controls = {
         form: null,
-        inputs: null
+        inputs: null,
+        info_block: null
     }
     Drupal.initOrderStep = function () {
         this.controls.form = $("form.photo-film-form");
         this.controls.inputs = $("input.required, select.required", this.controls.form);
+        this.controls.info_block = $("div.info-block-wrap", this.controls.form);
 
         $("#edit-mobile").mask("(999) 999-99-99");
         this.controls.inputs.on("keyup change", $.proxy(this.enableSubmitButton, this));
+
+        $("a.open-info", this.controls.info_block).on("click", $.proxy(this.openInfoBlock, this));
+        $("a.close-info", this.controls.info_block).on("click", $.proxy(this.closeInfoBlock, this));
     }
 
     Drupal.enableSubmitButton = function () {
@@ -27,6 +32,20 @@
             $("div.form-button-wrap").addClass("active");
         } else {
             $("div.form-button-wrap").removeClass("active");
+        }
+    }
+
+    Drupal.openInfoBlock = function (e) {
+        e.preventDefault();
+        if (this.controls.info_block.height() == 0) {
+            this.controls.info_block.children("div.info-message").slideDown(200);
+        }
+    }
+
+    Drupal.closeInfoBlock = function (e) {
+        e.preventDefault();
+        if (!this.controls.info_block.height() == 0) {
+            this.controls.info_block.children("div.info-message").slideUp(200);
         }
     }
 
