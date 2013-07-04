7da6cb863a2254b456c3c6dbe6bb85d4cc6b5d76
sagaydak <sagaydakam@gmail.com>
2013-07-04 22:52:42 +0300

    04.07.13 | Photos Step - subtitles (css & js)

--- sites/all/modules/custom/photo_film_form/templates/photo-film-select-picture-step-content.tpl.php
+++ sites/all/modules/custom/photo_film_form/templates/photo-film-select-picture-step-content.tpl.php
@@ -26,8 +26,30 @@
     <div class="form-warning-message"><?php print $warning ?></div>
   <?php endif; ?>
 
+  <div class="subtitles-block initial-subtitles clearfix">
+    <a class="subtitles-button" href=""><?php print t("Initial subtitles") ?></a>
+    <span class="subtitles-desc"><?php print t("You can add text for initial subtitles in your movie") ?></span>
+    <hr class="separator" />
+    <div class="subtitles-body hidden">
+      <textarea rows="5" cols="15" name="subtitles_initial"></textarea>
+      <span><?php print t("This field is not required and can be left empty if you want") ?></span>
+    </div>
+  </div>
+
   <ul class="user-photos-wrap clearfix">
+    <h2><?php print t("You didn't upload photos yet") ?></h2>
   </ul>
+
+  <div class="subtitles-block final-subtitles clearfix">
+    <div class="subtitles-body hidden">
+      <textarea rows="5" cols="15" name="subtitles_final"></textarea>
+      <span><?php print t("This field is not required and can be left empty if you want") ?></span>
+    </div>
+    <hr class="separator" />
+    <a class="subtitles-button" href=""><?php print t("Final subtitles") ?></a>
+    <span class="subtitles-desc"><?php print t("Also you can add text for final subtitles that will be placed at the end of your movie") ?></span>
+  </div>
+
 
   <div id="comment-dialog" class="hidden" title="<?php print t("Comment photo") ?>">
     <textarea name="comment" id="comment" class="text ui-widget-content"></textarea>
