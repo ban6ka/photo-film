f07de2d051e61604ee1bcb94f59682bbfeaf86a6
Ivan Goncharenko <Ivan.Goncharenko@propeople.com.ua>
2014-02-04 20:41:49 +0200

    added #content to all form actions and crumbs

--- sites/all/modules/custom/photo_film_form/templates/photo-film-form-form-navigation.tpl.php
+++ sites/all/modules/custom/photo_film_form/templates/photo-film-form-form-navigation.tpl.php
@@ -13,18 +13,20 @@
   $is_active = TRUE; ?>
   <ul class="photo-film-navigation">
     <?php foreach ($steps as $key => $single_step):
-    if ($key === $current) :
-      $is_active = FALSE;
-    endif;
-    if ($is_active):
-      $single_step['attributes'] = array('attributes' => array('class' => array('active')));
-    endif ?>
-    <li class="<?php print ($is_active ? "active-step " : "") ?> clearfix">
-      <?php print l(t($single_step['title']), $single_step['href'], $single_step['attributes']) ?>
-      <?php if (end($steps) != $single_step) : ?>
-        <span class="navigation-separator">&nbsp;</span>
-      <?php endif ?>
-    </li>
+      $attributes['fragment'] = 'content';
+      $attributes['attributes']['name'] = 'content';
+      if ($key === $current) :
+        $is_active = FALSE;
+      endif;
+      if ($is_active):
+        $single_step['attributes'] = array('fragment' => 'content', 'attributes' => array('class' => array('active'), 'name' => 'content'));
+      endif ?>
+      <li class="<?php print ($is_active ? "active-step " : "") ?> clearfix">
+        <?php print l(t($single_step['title']), $single_step['href'], !empty($single_step['attributes']) ? $single_step['attributes'] : $attributes) ?>
+        <?php if (end($steps) != $single_step) : ?>
+          <span class="navigation-separator">&nbsp;</span>
+        <?php endif ?>
+      </li>
     <?php endforeach; ?>
   </ul>
 <?php endif ?>
